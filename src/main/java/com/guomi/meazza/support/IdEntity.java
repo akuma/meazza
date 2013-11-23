062997acca5f7aa7d72b5e19c0c8062a2c81f20c
Akuma Huang <ihuangwj@gmail.com>
2013-11-23 14:40:27 +0800

    添加 equals()、hashCode() 方法

--- src/main/java/com/guomi/meazza/support/IdEntity.java
+++ src/main/java/com/guomi/meazza/support/IdEntity.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * @(#)IdEntity.java    Created on 2012-8-2
  * Copyright (c) 2012 Guomi. All rights reserved.
  */
@@ -6,6 +6,8 @@ package com.guomi.meazza.support;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -32,6 +34,16 @@ public abstract class IdEntity<T> implements Serializable {
 
     public void setId(T id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
     }
 
     @Override
