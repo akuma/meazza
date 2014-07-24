2729e572a6c18e3e813d8cc9f6994166b3137c9e
Akuma Huang <ihuangwj@gmail.com>
2014-07-24 17:32:29 +0800

    添加 SqlUtils.splitSql(String, boolean)

--- src/main/java/com/guomi/meazza/util/SqlUtils.java
+++ src/main/java/com/guomi/meazza/util/SqlUtils.java
@@ -1,9 +1,15 @@
-/* 
+/*
  * @(#)SqlUtils.java    Created on 2013-7-25
  * Copyright (c) 2013 Guomi. All rights reserved.
  */
 package com.guomi.meazza.util;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -35,6 +41,51 @@ public abstract class SqlUtils {
             countSql = SQL_FIRST_SELECT_PREFIX_PATTERN.matcher(originSql).replaceFirst(SQL_SELECT_COUNT_PREFIX);
         }
         return countSql;
+    }
+
+    /**
+     * 解析 sql 文本，把所有 sql 作为结果返回。
+     */
+    public static String[] splitSql(String sqlText, boolean slashComments) throws IOException {
+        if (!sqlText.endsWith(";")) {
+            sqlText += ";";
+        }
+
+        Reader r = new StringReader(sqlText);
+        StreamTokenizer token = new StreamTokenizer(r);
+
+        token.resetSyntax();// 重置此标记生成器的语法表，使所有字符都成为“普通”字符。
+        token.slashStarComments(slashComments);// 确定标记生成器是否识别 C 样式注释。
+        token.quoteChar('\'');// 指定此字符的匹配对分隔此标记生成器中的字符串常量
+
+        List<String> sqlList = new ArrayList<String>();
+        StringBuffer sb = new StringBuffer();
+
+        int flag = StreamTokenizer.TT_EOF;
+        while (StreamTokenizer.TT_EOF != (flag = token.nextToken())) {
+            switch (flag) {
+            case StreamTokenizer.TT_NUMBER:
+                sb.append(token.nval);
+                break;
+            case StreamTokenizer.TT_WORD:
+                sb.append(token.sval);
+                break;
+            case '\'':
+                sb.append('\'').append(token.sval).append('\'');
+                break;
+            case ';':
+                if ((!sb.toString().trim().startsWith("--") || !slashComments)
+                        && !StringUtils.isBlank(sb.toString().trim())) {
+                    sqlList.add(sb.toString());
+                }
+                sb = new StringBuffer();
+                break;
+            default:
+                sb.append((char) token.ttype);
+                break;
+            }
+        }
+        return sqlList.toArray(new String[sqlList.size()]);
     }
 
 }
