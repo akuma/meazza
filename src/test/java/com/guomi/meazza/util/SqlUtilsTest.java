dba4fc9c4326e6956728c7f7cff58e05be93cdbb
Akuma Huang <ihuangwj@gmail.com>
2014-12-26 15:13:05 +0800

    注释部分代码

--- src/test/java/com/guomi/meazza/util/SqlUtilsTest.java
+++ src/test/java/com/guomi/meazza/util/SqlUtilsTest.java
@@ -6,9 +6,6 @@ package com.guomi.meazza.util;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
-
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 /**
@@ -95,15 +92,15 @@ public class SqlUtilsTest {
         elapsed = System.currentTimeMillis() - start;
         System.out.println(elapsed);
 
-        try {
-            originSql = IOUtils.toString(SqlUtilsTest.class.getResourceAsStream("testsql.txt"));
-            System.out.println(originSql);
-        } catch (IOException e) {
-        }
-        countSql = SqlUtils.generateCountSql(originSql);
-        System.out.println(countSql);
-        elapsed = System.currentTimeMillis() - start;
-        System.out.println(elapsed);
+        //        try {
+        //            originSql = IOUtils.toString(SqlUtilsTest.class.getResourceAsStream("testsql.txt"));
+        //            System.out.println(originSql);
+        //        } catch (IOException e) {
+        //        }
+        //        countSql = SqlUtils.generateCountSql(originSql);
+        //        System.out.println(countSql);
+        //        elapsed = System.currentTimeMillis() - start;
+        //        System.out.println(elapsed);
     }
 
 }
