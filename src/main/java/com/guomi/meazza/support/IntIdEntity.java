f9190d6f6f2495f790debd1a234d9fb595b94543
Akuma Huang <ihuangwj@gmail.com>
2015-03-19 16:02:28 +0800

    添加 IntIdEntity 类

--- src/main/java/com/guomi/meazza/support/IntIdEntity.java
+++ src/main/java/com/guomi/meazza/support/IntIdEntity.java
@@ -1,31 +1,29 @@
 /*
- * @(#)LongIdEntity.java    Created on 2013-1-12
- * Copyright (c) 2013 Guomi. All rights reserved.
+ * @(#)IntIdEntity.java    Created on 2015年3月19日
+ * Copyright (c) 2015 Guomi. All rights reserved.
  */
 package com.guomi.meazza.support;
 
 import java.util.Date;
 
 /**
- * ID 为 Long 类型的 Entity 基类。
+ * ID 为 Integer 类型的 Entity 基类。
  *
  * @author akuma
  */
-public abstract class LongIdEntity extends IdEntity {
+public class IntIdEntity extends IdEntity {
 
     private static final long serialVersionUID = -8056033887637038214L;
 
-    // 对于数据库中是 int 类型的 ID 字段，如果在实体类中定义为 Long，而在 mybatis 配置中又不将 id column 设置为 long，
-    // 则获取到的 ID 类型将是 Integer 类型，所以在这个子类中需要重新定义 id
-    private Long id;
+    private Integer id;
     private Date modifyTime;
     private Date creationTime;
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
