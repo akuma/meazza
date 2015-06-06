2edfbf52b7771cd7f633c25a3d2a5cd8375bf484
Akuma Huang <ihuangwj@gmail.com>
2015-06-06 19:28:54 +0800

    升级 spring v4.1.6

--- src/test/java/com/guomi/meazza/orm/mybatis/DemoDao.java
+++ src/test/java/com/guomi/meazza/orm/mybatis/DemoDao.java
@@ -1,13 +1,11 @@
-/* 
+/*
  * @(#)DemoDao.java    Created on 2012-8-1
  * Copyright (c) 2012 Guomi. All rights reserved.
  */
 package com.guomi.meazza.orm.mybatis;
 
-
 /**
  * @author akuma
  */
-public class DemoDao extends DefaultMyBatisBasicDao {
-
+public class DemoDao extends DefaultMyBatisBasicDao<Object> {
 }
