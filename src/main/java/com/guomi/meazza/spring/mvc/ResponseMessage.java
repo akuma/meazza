6cf9fe9d37b5cfbfed631d2d8168ff2b0c6c9e0e
Akuma Huang <ihuangwj@gmail.com>
2015-08-28 18:54:07 +0800

    添加 code 属性，表示响应代码

--- src/main/java/com/guomi/meazza/spring/mvc/ResponseMessage.java
+++ src/main/java/com/guomi/meazza/spring/mvc/ResponseMessage.java
@@ -13,22 +13,31 @@ import java.util.Map;
 /**
  * Controller 操作的响应消息类，主要用在以 <code>ResponseBody</code> 方式返回结果的方法中。<br>
  * 它包含了 Action 提示信息、Action 错误信息、字段错误信息以及其他响应数据。
- * 
+ *
  * @author akuma
  */
 public class ResponseMessage implements Serializable {
 
     private static final long serialVersionUID = 6725085407815077443L;
 
+    private long code = 0; // 响应代码，默认 0 表示成功
     private Collection<String> actionMessages = new ArrayList<>(0);
     private Collection<String> actionErrors = new ArrayList<>(0);
     private Map<String, Collection<String>> fieldErrors = new HashMap<>(0);
 
     private Map<String, Object> data = new HashMap<>(); // 其他需要返回的数据
 
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
     /**
      * 获取是否存在 Action 消息。
-     * 
+     *
      * @return true/false
      */
     public boolean getHasActionMessages() {
@@ -65,7 +74,7 @@ public class ResponseMessage implements Serializable {
 
     /**
      * 添加需要返回给客户端的属性。
-     * 
+     *
      * @param name
      *            属性名称
      * @param value
@@ -77,7 +86,7 @@ public class ResponseMessage implements Serializable {
 
     /**
      * 批量添加需要返回给客户端的属性。
-     * 
+     *
      * @param data
      *            属性 map
      */
@@ -87,7 +96,7 @@ public class ResponseMessage implements Serializable {
 
     /**
      * 获取是否存在 Action 或者 Field 错误。
-     * 
+     *
      * @return true/false
      */
     public boolean getHasErrors() {
@@ -96,7 +105,7 @@ public class ResponseMessage implements Serializable {
 
     /**
      * 获取是否存在 Action 错误。
-     * 
+     *
      * @return true/false
      */
     public boolean getHasActionErrors() {
@@ -105,7 +114,7 @@ public class ResponseMessage implements Serializable {
 
     /**
      * 获取是否存在 Field 错误。
-     * 
+     *
      * @return true/false
      */
     public boolean getHasFieldErrors() {
