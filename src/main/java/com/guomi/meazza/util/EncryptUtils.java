f09fe260ef01afcbf9e25a4577194b0207db6d5b
Akuma Huang <ihuangwj@gmail.com>
2015-12-15 14:06:24 +0800

    添加 rsa、sha1 等签名验证方法

--- src/main/java/com/guomi/meazza/util/EncryptUtils.java
+++ src/main/java/com/guomi/meazza/util/EncryptUtils.java
@@ -4,10 +4,13 @@
  */
 package com.guomi.meazza.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.Signature;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
@@ -18,6 +21,7 @@ import javax.crypto.spec.DESedeKeySpec;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,6 +42,9 @@ public abstract class EncryptUtils {
 
     // RSA 加密算法
     private static final String ALGORITHM_RSA = "RSA";
+
+    // RSA 签名算法
+    private static final String ALGORITHM_RSA_SIG = "SHA1withRSA";
 
     private static final String DEFAULT_CHARSET = "UTF-8";
 
@@ -217,7 +224,7 @@ public abstract class EncryptUtils {
 
             Cipher cipher = Cipher.getInstance(ALGORITHM_DES);
             cipher.init(Cipher.DECRYPT_MODE, deskey);
-            byte[] clearText = cipher.doFinal(Base64.decodeBase64(str.getBytes()));
+            byte[] clearText = cipher.doFinal(Base64.decodeBase64(str));
             decoded = new String(clearText);
         } catch (Exception e) {
             logger.error("Decode by 3DES & Base64 error", e);
@@ -227,7 +234,7 @@ public abstract class EncryptUtils {
     }
 
     /**
-     * 使用 3DES 加密，然后使用 Base64 编码。
+     * 使用 RSA 加密，然后使用 Base64 编码。
      *
      * @param str
      *            源字符串
@@ -240,14 +247,22 @@ public abstract class EncryptUtils {
         String encoded = null;
 
         try {
-            byte[] byteKey = Base64.decodeBase64(publicKey.getBytes());
-            X509EncodedKeySpec X509publicKey = new X509EncodedKeySpec(byteKey);
-            KeyFactory kf = KeyFactory.getInstance(ALGORITHM_RSA);
-            PublicKey pubKey = kf.generatePublic(X509publicKey);
+            byte[] byteKey = Base64.decodeBase64(publicKey);
+            KeyFactory factory = KeyFactory.getInstance(ALGORITHM_RSA);
+            RSAPublicKey pubKey = (RSAPublicKey) factory.generatePublic(new X509EncodedKeySpec(byteKey));
 
             Cipher cipher = Cipher.getInstance(ALGORITHM_RSA);
             cipher.init(Cipher.ENCRYPT_MODE, pubKey);
-            byte[] cipherText = cipher.doFinal(str.getBytes());
+
+            // 加密时超过 blockSize 字节就报错，为此采用分段加密
+            int blockSize = pubKey.getModulus().bitLength() / 8 - 11;
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] data = str.getBytes();
+            for (int i = 0; i < data.length; i += blockSize) {
+                byte[] doFinal = cipher.doFinal(ArrayUtils.subarray(data, i, i + blockSize));
+                out.write(doFinal);
+            }
+            byte[] cipherText = out.toByteArray();
             encoded = new String(Base64.encodeBase64(cipherText));
         } catch (Exception e) {
             logger.error("Encode by RSA & Base64 error", e);
@@ -270,20 +285,89 @@ public abstract class EncryptUtils {
         String decoded = null;
 
         try {
-            byte[] byteKey = Base64.decodeBase64(privateKey.getBytes());
-            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(byteKey);
-            KeyFactory fact = KeyFactory.getInstance(ALGORITHM_RSA);
-            PrivateKey priveKey = fact.generatePrivate(keySpec);
+            byte[] byteKey = Base64.decodeBase64(privateKey);
+            KeyFactory factory = KeyFactory.getInstance(ALGORITHM_RSA);
+            RSAPrivateKey priveKey = (RSAPrivateKey) factory.generatePrivate(new PKCS8EncodedKeySpec(byteKey));
 
             Cipher cipher = Cipher.getInstance(ALGORITHM_RSA);
             cipher.init(Cipher.DECRYPT_MODE, priveKey);
-            byte[] clearText = cipher.doFinal(Base64.decodeBase64(str.getBytes()));
+
+            // 解密时超过 blockSize 字节就报错，为此采用分段加密
+            int blockSize = priveKey.getModulus().bitLength() / 8;
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] data = Base64.decodeBase64(str);
+            for (int i = 0; i < data.length; i += blockSize) {
+                byte[] doFinal = cipher.doFinal(ArrayUtils.subarray(data, i, i + blockSize));
+                out.write(doFinal);
+            }
+            byte[] clearText = out.toByteArray();
             decoded = new String(clearText);
         } catch (Exception e) {
             logger.error("Decode by RSA & Base64 error", e);
         }
 
         return decoded;
+    }
+
+    /**
+     * 使用 RSA 签名，然后使用 Base64 编码。
+     *
+     * @param str
+     *            源字符串
+     * @param privateKey
+     *            私钥
+     * @return 字符串的签名结果
+     * @see {@link #verifyByRSA(String, String, String)}
+     */
+    public static String signByRSA(String str, String privateKey) {
+        String result = null;
+
+        try {
+            KeyFactory factory = KeyFactory.getInstance(ALGORITHM_RSA);
+            byte[] byteKey = Base64.decodeBase64(privateKey);
+            PrivateKey priveKey = factory.generatePrivate(new PKCS8EncodedKeySpec(byteKey));
+
+            Signature sig = Signature.getInstance(ALGORITHM_RSA_SIG);
+            sig.initSign(priveKey);
+            sig.update(str.getBytes());
+            byte[] signed = sig.sign();
+            result = new String(Base64.encodeBase64(signed));
+        } catch (Exception e) {
+            logger.error("Sign by RSA error", e);
+        }
+
+        return result;
+    }
+
+    /**
+     * 使用 Base64 解码，然后使用 RSA 公钥进行签名验证。
+     *
+     * @param str
+     *            签名
+     * @param publicKey
+     *            公钥
+     * @param expectSign
+     *            期望签名结果
+     * @return 签名是否验证通过
+     * @see {@link #signByRSA(String, String)}
+     */
+    public static boolean verifyByRSA(String str, String publicKey, String expectSign) {
+        boolean isSuccess = false;
+
+        try {
+            KeyFactory factory = KeyFactory.getInstance(ALGORITHM_RSA);
+            byte[] byteKey = Base64.decodeBase64(publicKey);
+            RSAPublicKey pubKey = (RSAPublicKey) factory.generatePublic(new X509EncodedKeySpec(byteKey));
+
+            Signature sig = Signature.getInstance(ALGORITHM_RSA_SIG);
+            sig.initVerify(pubKey);
+            sig.update(str.getBytes());
+            isSuccess = sig.verify(Base64.decodeBase64(expectSign));
+        } catch (Exception e) {
+            logger.error("Verify by RSA error", e);
+        }
+
+        return isSuccess;
     }
 
     /**
@@ -308,7 +392,7 @@ public abstract class EncryptUtils {
     }
 
     /**
-     * 验证字符串签名是否匹配。加密算法为：md5(str + secure)
+     * 验证字符串签名是否匹配。签名算法为：SHA1(str + secure)
      *
      * @param str
      *            需要签名的字符串
@@ -318,10 +402,44 @@ public abstract class EncryptUtils {
      *            期望签名结果
      * @return true/false
      */
-    public static boolean verifyMd5Hex(String str, String secure, String expectSign) {
+    public static boolean verifyBySHA1(String str, String secure, String expectSign) {
+        String secureStr = str + secure;
+        String actualSign = DigestUtils.sha1Hex(getBytes(secureStr, DEFAULT_CHARSET));
+        return actualSign.equals(expectSign);
+    }
+
+    /**
+     * 验证字符串签名是否匹配。签名算法为：MD5(str + secure)
+     *
+     * @param str
+     *            需要签名的字符串
+     * @param secure
+     *            密钥
+     * @param expectSign
+     *            期望签名结果
+     * @return true/false
+     */
+    public static boolean verifyByMD5(String str, String secure, String expectSign) {
         String secureStr = str + secure;
         String actualSign = DigestUtils.md5Hex(getBytes(secureStr, DEFAULT_CHARSET));
         return actualSign.equals(expectSign);
+    }
+
+    /**
+     * 验证字符串签名是否匹配。签名算法为：MD5(str + secure)
+     *
+     * @param str
+     *            需要签名的字符串
+     * @param secure
+     *            密钥
+     * @param expectSign
+     *            期望签名结果
+     * @return true/false
+     * @deprecated 请使用 {@link #verifyByMD5(String, String, String)}
+     */
+    @Deprecated
+    public static boolean verifyMd5Hex(String str, String secure, String expectSign) {
+        return verifyByMD5(str, secure, expectSign);
     }
 
     private static int sumSqual(byte[] b) {
--- src/test/java/com/guomi/meazza/util/EncryptUtilsTest.java
+++ src/test/java/com/guomi/meazza/util/EncryptUtilsTest.java
@@ -5,6 +5,7 @@
 package com.guomi.meazza.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -21,17 +22,30 @@ public class EncryptUtilsTest {
     @Test
     public void testRSA() throws NoSuchAlgorithmException {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(512);
+        keyGen.initialize(1024);
         KeyPair key = keyGen.generateKeyPair();
         String publicKey = Base64.encodeBase64String(key.getPublic().getEncoded());
         String privateKey = Base64.encodeBase64String(key.getPrivate().getEncoded());
-        System.out.println(publicKey);
-        System.out.println(privateKey);
+        System.out.println("publicKey: " + publicKey);
+        System.out.println("privateKey: " + privateKey);
 
         String plainText = "Hello RSA!";
         String encodeText = EncryptUtils.encodeByRSAAndBase64(plainText, publicKey);
         String decodeText = EncryptUtils.decodeByRSAAndBase64(encodeText, privateKey);
         assertEquals(plainText, decodeText);
+
+        plainText = "body=学习周卡&buyer_email=xingken@126.com&buyer_id=2088002097358275&discount=0.00"
+                + "&gmt_create=2015-12-14 19:22:57&gmt_payment=2015-12-14 19:22:58&is_total_fee_adjust=N"
+                + "&notify_id=47b6a74eb3ea24ea1cc4de4995ed5a0f3i&notify_time=2015-12-14 19:26:15"
+                + "&notify_type=trade_status_sync&out_trade_no=402881e5519ffd600151a03a84b5015b&payment_type=1"
+                + "&price=0.01&quantity=1&seller_email=zdyaohm@163.com&seller_id=2088111858244206&subject=学习周卡"
+                + "&total_fee=0.01&trade_no=2015121400001000270005361312&trade_status=TRADE_SUCCESS&use_coupon=N";
+        encodeText = EncryptUtils.encodeByRSAAndBase64(plainText, publicKey);
+        decodeText = EncryptUtils.decodeByRSAAndBase64(encodeText, privateKey);
+        assertEquals(plainText, decodeText);
+
+        String sign = EncryptUtils.signByRSA(plainText, privateKey);
+        assertTrue(EncryptUtils.verifyByRSA(plainText, publicKey, sign));
     }
 
 }
